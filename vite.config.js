dac1360004270614da61cfce77b9632ff56179c9
lee <djqan4631@gmail.com>
2026-02-10 23:22:02 +0900

    최신

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-stock-dashboard',
   server: {
     proxy: {
       // '/api'로 시작하는 요청이 오면, Vite가 대신 구글 서버로 갑니다.
